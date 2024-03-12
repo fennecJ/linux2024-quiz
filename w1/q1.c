6abddc780f42f44eaaf8c7bf8a5a96c44b9e4dfd
fennecJ <hwahwa649@gmail.com>
2024-03-13 04:05:19 +0800

    Implement permuteAndSort to statistic max_depth
    The function permuteAndSort will find all permutations of a given array
    and use each permutation to build a list then pass the list to
    quick_sort function. This can be convinient for us to statistic the
    possible maximum depth for begin[] array.
    
    Mind that this function assume every element in the array is unique.

--- w1/q1.c
+++ w1/q1.c
@@ -54,13 +54,14 @@ static inline void rebuild_list_link(struct list_head *head)
     prev->next = head;
     head->prev = prev;
 }
-
+int max_cnt[30]  = {0};
 
 void quick_sort(struct list_head *list)
 {
     int n = list_length(list);
     int value;
     int i = 0;
+    int max_depth = 0;
     int max_level = 2 * n;
     struct list_head *begin[max_level];
     struct list_head *result = NULL, *left = NULL, *right = NULL;
@@ -91,6 +92,8 @@ void quick_sort(struct list_head *list)
             begin[i] = left;
             begin[i + 1] = pivot;
             begin[i + 2] = right;
+            if(i > max_depth)
+                max_depth = i;
             left = right = NULL;
             i += 2;
 
@@ -104,6 +107,7 @@ void quick_sort(struct list_head *list)
     }
     list->next = result;
     rebuild_list_link(list);
+    max_cnt[max_depth]++;
     // list = head_record;
 }
 
@@ -136,6 +140,56 @@ void shuffle(int *array, size_t n)
     }
 }
 
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permuteAndSort(int* a, int l, int r, int len, struct list_head *list) 
+{ 
+    int i;
+    if (l == r) {
+        while (len--)
+            list_construct(list, a[len]);
+        quick_sort(list);
+        list_free(list);
+        INIT_LIST_HEAD(list);
+    }
+    else { 
+        for (i = l; i <= r; i++) {
+            swap((a + l), (a + i));
+            permuteAndSort(a, l + 1, r, len, list);
+            swap((a + l), (a + i));
+        }
+    }
+}
+
+int fact(int k){
+    int a = 1;
+    while (k > 0) {
+        a *= k;
+        k--;
+    }
+    return a;
+}
+
+void statistic(struct list_head *list, int len){
+    int *arr = malloc(sizeof(int) * len);
+    for(int i = 0; i < len; i++){
+        arr[i] = i + 1;
+    }
+    int per = fact(len);
+    permuteAndSort(arr, 0, len - 1, len, list); 
+
+    for(int i = 0; i < 30; i++){
+        if(max_cnt[i] > 0)
+            printf("%2d depth prob: %10f = %8d / %8d\n", i+2, max_cnt[i]/(double)per, max_cnt[i], per);
+    }
+}
+
 int main(int argc, char **argv)
 {
     struct list_head *list = malloc(sizeof(struct list_head));
@@ -144,14 +198,20 @@ int main(int argc, char **argv)
     size_t count = 100000;
     int *test_arr = malloc(sizeof(int) * count);
     for (int i = 0; i < count; ++i)
-        test_arr[i] = i;
+    test_arr[i] = i;
     shuffle(test_arr, count);
-
+    
     while (count--)
-        list_construct(list, test_arr[count]);
+    list_construct(list, test_arr[count]);
     quick_sort(list);
     assert(list_is_ordered(list));
     list_free(list);
     free(test_arr);
+    if(argc == 2){
+        INIT_LIST_HEAD(list);
+        int len = atoi(argv[1]);
+        statistic(list, len);
+    }
+    free(list);
     return 0;
 }
