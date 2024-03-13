706b3a5285af447fd698ba120e9e1fa03e91dff4
fennecJ <hwahwa649@gmail.com>
2024-03-13 21:48:48 +0800

    Compare time elapsed w/ and w/o list_tail

--- w1/q1/q1.c
+++ w1/q1/q1.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
 #include "list.h"
+#include "utils.h"
 typedef struct __node {
     long value;
     struct list_head list;
@@ -121,6 +123,57 @@ void quick_sort(struct list_head *list)
     // list = head_record;
 }
 
+void quick_sort_with_tail(struct list_head *list)
+{
+    int n = list_length(list);
+    int value;
+    int i = 0;
+    int max_level = 2 * n;
+    struct list_head *begin[max_level];
+    struct list_head *result = NULL, *left = NULL, *right = NULL;
+    struct list_head *head_record = list;
+    begin[0] = list->next;
+    list->prev->next = NULL;
+    while (i >= 0) {
+        struct list_head *L = begin[i], *R = list_tail(begin[i]);
+        if (L != R) {
+            struct list_head *pivot = L;
+            value = list_entry(pivot, node_t, list)->value;
+            struct list_head *p = pivot->next;
+            pivot->next = NULL; // break the list
+    
+            while (p) {
+                struct list_head *n = p;
+                p = p->next;
+                int n_value = list_entry(n, node_t, list)->value;
+                if(n_value > value) {
+                    n->next = right;
+                    right = n;
+                } else {
+                    n->next = left;
+                    left = n;
+                }
+            }
+
+            begin[i] = left;
+            begin[i + 1] = pivot;
+            begin[i + 2] = right;
+            left = right = NULL;
+            i += 2;
+
+        } else {
+            if (L){
+                L->next = result;
+                result = L;
+            }
+            i--;
+        }
+    }
+    list->next = result;
+    rebuild_list_link(list);
+    // list = head_record;
+}
+
 /* Verify if list is order */
 static bool list_is_ordered(struct list_head *head)
 {   
@@ -150,77 +203,64 @@ void shuffle(int *array, size_t n)
     }
 }
 
-void swap(int *x, int *y)
-{
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-void permuteAndSort(int* a, int l, int r, int len, struct list_head *list) 
-{ 
-    int i;
-    if (l == r) {
-        while (len--)
-            list_construct(list, a[len]);
-        quick_sort(list);
-        list_free(list);
-        INIT_LIST_HEAD(list);
-    }
-    else { 
-        for (i = l; i <= r; i++) {
-            swap((a + l), (a + i));
-            permuteAndSort(a, l + 1, r, len, list);
-            swap((a + l), (a + i));
-        }
-    }
-}
-
-int fact(int k){
-    int a = 1;
-    while (k > 0) {
-        a *= k;
-        k--;
-    }
-    return a;
-}
-
-void statistic(struct list_head *list, int len){
-    int *arr = malloc(sizeof(int) * len);
-    for(int i = 0; i < len; i++){
-        arr[i] = i + 1;
-    }
-    int per = fact(len);
-    permuteAndSort(arr, 0, len - 1, len, list); 
-
-    for(int i = 0; i < 30; i++){
-        if(max_cnt[i] > 0)
-            printf("%2d depth prob: %10f = %8d / %8d\n", i+2, max_cnt[i]/(double)per, max_cnt[i], per);
-    }
-}
 
 int main(int argc, char **argv)
 {
     struct list_head *list = malloc(sizeof(struct list_head));
     INIT_LIST_HEAD(list);
-
-    size_t count = 100000;
+    size_t arr_len = 100000;
+    size_t count = arr_len;
     int *test_arr = malloc(sizeof(int) * count);
-    for (int i = 0; i < count; ++i)
-    test_arr[i] = i;
-    shuffle(test_arr, count);
     
-    while (count--)
-    list_construct(list, test_arr[count]);
-    quick_sort(list);
-    assert(list_is_ordered(list));
-    list_free(list);
+    for (int i = 0; i < count; ++i)
+        test_arr[i] = i;
+
+    double elapse = 0.0;
+    for(int i = 0; i < 40; i++){
+        count = arr_len;
+        shuffle(test_arr, count);
+        while (count--)
+            list_construct(list, test_arr[count]);
+        double start_time = clock() / (double) CLOCKS_PER_SEC;
+        quick_sort(list);
+        double end_time = clock() / (double) CLOCKS_PER_SEC;
+        elapse += end_time - start_time;
+        assert(list_is_ordered(list));
+        list_free(list);
+        INIT_LIST_HEAD(list);
+    }
+    printf("Sort w/o tail: %6f\n", elapse);
+    elapse = 0.0;
+    for(int i = 0; i < 40; i++){
+        count = arr_len;
+        shuffle(test_arr, count);
+        while (count--)
+            list_construct(list, test_arr[count]);
+        double start_time = clock() / (double) CLOCKS_PER_SEC;
+        quick_sort_with_tail(list);
+        double end_time = clock() / (double) CLOCKS_PER_SEC;
+        elapse += end_time - start_time;
+        assert(list_is_ordered(list));
+        list_free(list);
+        INIT_LIST_HEAD(list);
+    }
+    printf("Sort w/  tail: %6f\n", elapse);
+
     free(test_arr);
     if(argc == 2){
         INIT_LIST_HEAD(list);
         int len = atoi(argv[1]);
-        statistic(list, len);
+        printf("permutation statistics with arr len = %d:\n", len);
+        double start_time = clock() / (double) CLOCKS_PER_SEC;
+        statistic(list, len, quick_sort);
+        double end_time = clock() / (double) CLOCKS_PER_SEC;
+        printf("Sort w/o tail: %6f\n", end_time - start_time);
+        
+        start_time = clock() / (double) CLOCKS_PER_SEC;
+        statistic(list, len, quick_sort_with_tail);
+        end_time = clock() / (double) CLOCKS_PER_SEC;
+        printf("Sort w/ tail: %6f\n", end_time - start_time);
+
     }
     free(list);
     return 0;
